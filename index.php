336dae49a73a0240a91e511a33ff9d9b8ebfb170
WSyncType <110849822+WSyncType@users.noreply.github.com>
2022-08-13 17:58:05 -0300

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<?php
 
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	  <title>WSyncType Segurity</title>
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  
+  <body>
+    
+  </body>
+</html>
